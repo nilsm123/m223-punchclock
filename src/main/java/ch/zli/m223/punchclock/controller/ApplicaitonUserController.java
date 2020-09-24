67589087c0d05aeb054776fc7d7abb17ecd773e3
zli <madnil059@gmail.com>
2020-09-24 04:54:32 -0700

    Implemented Controller, Repository and Service for ApplicationUser and CheckInLocation

---
+++ src/main/java/ch/zli/m223/punchclock/controller/ApplicaitonUserController.java
@@ -0,0 +1,29 @@
+package ch.zli.m223.punchclock.controller;
+
+import ch.zli.m223.punchclock.domain.User;
+import ch.zli.m223.punchclock.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class ApplicaitonUserController {
+
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public ApplicaitonUserController(UserRepository applicationUserRepository,
+                                     BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = applicationUserRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+}
